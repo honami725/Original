733870813ed77c3d60c06151e30397f2536e627b
Honami <honami.725725@gmail.com>
2016-02-19 00:00:47 +0900

    カメラ、アルバム起動

---
+++ Original/DinnerViewController.swift
@@ -0,0 +1,67 @@
+//
+//  DinnerViewController.swift
+//  PhotoMaster
+//
+//  Created by Honami on 2016/02/11.
+//  Copyright © 2016年 Honami. All rights reserved.
+//
+
+import UIKit
+
+class DinnerViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    @IBOutlet var photoImageView : UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func precentPickerContoroller(sourceType: UIImagePickerControllerSourceType){
+        
+        if UIImagePickerController.isSourceTypeAvailable(sourceType){
+            let picker = UIImagePickerController()
+            picker.sourceType = sourceType
+            picker.delegate = self
+            self.presentViewController(picker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: NSDictionary!) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        photoImageView.image = image
+    }
+    
+    @IBAction func selectButtonTapped(sender: UIButton){
+        let alertController = UIAlertController(title: "画像の取得先を選択", message: nil, preferredStyle: .ActionSheet)
+        
+        let firstAction = UIAlertAction(title: "カメラ", style: .Default){
+            action in
+            self.precentPickerContoroller(.Camera)
+        }
+        let secondAction = UIAlertAction(title: "アルバム", style: .Default){
+            action in
+            self.precentPickerContoroller(.PhotoLibrary)
+        }
+        let cancelAction = UIAlertAction(title: "キャンセル", style: .Cancel, handler: nil)
+        
+        alertController.addAction(firstAction)
+        alertController.addAction(secondAction)
+        alertController.addAction(cancelAction)
+        
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
