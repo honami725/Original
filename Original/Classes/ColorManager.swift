f6919ccc119358aca690a5f360ebd3bc73459d1b
masuhara <info@masuhara.net>
2016-02-19 01:22:58 +0900

    Add ColorManager

---
+++ Original/Classes/ColorManager.swift
@@ -0,0 +1,19 @@
+//
+//  ColorManager.swift
+//  Original
+//
+//  Created by Masuhara on 2016/02/19.
+//  Copyright © 2016年 Honami. All rights reserved.
+//
+
+import UIKit
+
+class ColorManager {
+    class func facebookMessengerBlue() -> UIColor {
+        return UIColor(red: 18.0 / 255.0, green: 104.0 / 255.0, blue: 249.0 / 255.0, alpha: 1.0)
+    }
+    
+    class func sampleColor() -> UIColor {
+        return UIColor(red: 200.0 / 255.0, green: 200.0 / 255.0, blue: 200.0 / 255.0, alpha: 1.0)
+    }
+}
